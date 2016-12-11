255ae744ec98b54ebbd390a2722be48beff45f3e
Justin Falcone <stanulam59@gmail.com>
2016-12-11 10:46:59 -0500

    logs messages

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "parser": "babel-eslint",
+    "extends": [
+        "standard",
+        "plugin:import/recommended",
+        "plugin:react/recommended",
+    ],
+    "plugins": [
+        "standard",
+        "promise",
+        "import",
+        "react",
+    ],
+    "rules": {
+        'indent':['error',4],
+        'comma-dangle':['error','always-multiline'],
+        'quotes':['error','double'],
+    }
+};
